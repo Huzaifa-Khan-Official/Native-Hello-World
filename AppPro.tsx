904215516f33c86de2a3a0c8b929177cf6229ea4
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-09-17 20:19:06 +0500

    1st project hello world done

--- AppPro.tsx
+++ AppPro.tsx
@@ -1,29 +1,38 @@
 import React from "react";
+import { SafeAreaView, StyleSheet, Text, useColorScheme, View } from "react-native";
 
-import { SafeAreaView, Text, View, StyleSheet, useColorScheme } from "react-native";
-
-function AppPro(): JSX.Element {
-    const isDarkMode = useColorScheme() === 'dark'
+function AppPro() {
+    const isDarkMode = useColorScheme() === "dark";
 
     return (
         <SafeAreaView>
             <View style={styles.container}>
-                <Text style={isDarkMode ? styles.whiteText : styles.darkText}>Paragraph 1:</Text>
+                <Text style={isDarkMode ? styles.whiteText : styles.darkText}>Heading 1</Text>
+                <Text>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus corrupti recusandae laboriosam quisquam inventore. Alias nostrum aut odit autem, id, minus maxime recusandae incidunt quis reiciendis inventore ut! Exercitationem, dolorum!
+                </Text>
             </View>
         </SafeAreaView>
-    )
+    );
 };
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        alignItems: "center"
+        padding: 10
     },
     whiteText: {
-        color: "white"
+        textAlign: 'center',
+        color: "white",
+        fontSize: 40,
+        fontWeight: "bold",
+        marginBottom: 20,
     },
     darkText: {
-        color: "black"
+        textAlign: 'center',
+        color: "black",
+        fontSize: 40,
+        fontWeight: "bold",
+        marginBottom: 20,
     }
 });
 
